e76fa53b90237034c8f83bfdcede7055aa38f833
Stesho <65865398+Stesho@users.noreply.github.com>
2020-06-17 01:26:12 +0300

    Add files via upload

---
+++ lab3/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Person
+    {
+        private string name;
+        private int height;
+        private string rank;
+        public static string country;
+
+        public Person() : this("No Info")
+        {
+        }
+        public Person(string a) : this(a, 0, "No Info")
+        {
+        }
+        public Person(string a, int b) : this(a, b, "No Info")
+        {
+        }
+        public Person(string a, string c) : this(a, 0, c)
+        {
+        }
+        public Person(int b, string c) : this("No Info", b, c)
+        {
+        }
+        public Person(string name, int weight, string country)
+        {
+            this.name = name;
+            this.height = weight;
+            this.rank = country;
+        }
+
+
+        public void SetValue(string name, int weight, string country)
+        {
+            this.name = name;
+            this.height = weight;
+            this.rank = country;
+        }
+        public void SetCountry(int val)
+        {
+            switch (val)
+            {
+                case 1:
+                    Person.country = "Germany";
+                    break;
+                case 2:
+                    Person.country = "Russia";
+                    break;
+                case 3:
+                    Person.country = "USA";
+                    break;
+                case 4:
+                    Person.country = "UK";
+                    break;
+                case 5:
+                    Person.country = "France";
+                    break;
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public string GetRank()
+        {
+            return rank;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {name} \nHeight: {height} \nRank: {rank}");
+        }
+
+    }
+
+    class Sportsman : Person
+    {
+        Person[] info;
+        public Sportsman(int a)
+        {
+            info = new Person[a];
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                return info[index];
+            }
+            set
+            {
+                info[index] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5, val;
+            Random rnd = new Random();
+            Sportsman Athlete = new Sportsman(num);
+
+            Athlete[0] = new Person("Alexey", 195, "Master");
+
+            Athlete[1] = new Person("Ivan", 195, "No Info");
+
+            Athlete[2] = new Person("Pavel", "Skilled");
+
+            Athlete[3] = new Person(195, "Professional");
+
+            Athlete[4] = new Person();
+            Athlete[4].SetValue("Ilya", 170, "beginner");
+
+            for(int i = 0; i < num; i++)
+            {
+                val = rnd.Next(1, 5);
+                Athlete[i].SetCountry(val);
+
+                Athlete[i].GetInfo();
+                Console.WriteLine($"Country: {Person.country}\n");
+            }
+        }
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace lab5
+{
+    enum Formating : int
+    {
+        Short = 0,
+        Standart = 1, 
+        All = 2
+    }
+
+    abstract class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+
+    abstract class Sportsmen : Person
+    {
+        public int Rank { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+
+        public Sportsmen(string name, int age, int rank, int weight, int height) : base(name, age)
+        {
+            this.Rank = rank;
+            this.Weight = weight;
+            this.Height = height;
+        }
+    }
+
+    class Master : Sportsmen
+    {
+        public string SportKind { get; set; }
+
+        public Master(string name, int age, int rank, int weight, int height, string sportKind) : base(name, age, rank, weight, height)
+        {
+            this.SportKind = sportKind;
+        }
+        public void Print(int format)
+        {
+            switch (format)
+            {
+                case (int)Formating.All:
+                    Console.WriteLine($"Name: {Name}\nAge: {Age}\nRank: {Rank}\nWeight: {Weight}\nHeight: {Height}\nKind of sport: {SportKind}\n");
+                    break;
+                case (int)Formating.Standart:
+                    Console.WriteLine($"Name: {Name}\nAge: {Age}\nRank: {Rank}\nWeight: {Weight}\nHeight: {Height}\n");
+                    break;
+                case (int)Formating.Short:
+                    Console.WriteLine($"Name: {Name}\nAge: {Age}\n");
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Master Athlete1 = new Master("Van Damn", 1, 2, 75, 177, "karate");
+            Athlete1.Print(0);
+
+            Master Athlete2 = new Master("Jackie Chan", 3, 4, 65, 174, "karate");
+            Athlete2.Print(1);
+
+            Master Athlete3 = new Master("DD", 5, 6, 75, 177, "GG");
+            Athlete3.Print(2);
+
+        }
+    }
+}
